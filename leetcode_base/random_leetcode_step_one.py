a0a573e716f336898d625f700e8f1e1d28ae3db9
wat1r <cnwangzhou@hotmail.com>
2019-11-12 23:02:04 +0800

    init 1st

--- leetcode_base/random_leetcode_step_one.py
+++ leetcode_base/random_leetcode_step_one.py
@@ -3,7 +3,25 @@ class RandomLeetcodeStepOne():
     def init(self):
         return ""
 
+    def minDistance(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+
+        def dp(i, j):
+            if i == -1: return j + 1
+            if j == -1: return i + 1
+
+            if word1[i] == word2[j]:
+                return dp(i - 1, j - 1)
+            else:
+                return min(dp(i, j - 1), dp(i - 1, j), dp(i - 1, j - 1)) + 1
+        return dp(len(word1) - 1, len(word2) - 1)
+
 
 if __name__ == '__main__':
     handler = RandomLeetcodeStepOne()
     # handler.init()
+    # handler.minDistance()
