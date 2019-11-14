ac2f9806cacf953bb054f8ac32656b9666f034bc
wat1r <cnwangzhou@hotmail.com>
2019-11-14 08:13:54 +0800

    coins

--- leetcode_base/random_leetcode_step_one.py
+++ leetcode_base/random_leetcode_step_one.py
@@ -21,21 +21,33 @@ class RandomLeetcodeStepOne():
                 return dp(i - 1, j - 1)
             else:
                 return min(dp(i, j - 1), dp(i - 1, j), dp(i - 1, j - 1)) + 1
+
         return dp(len(word1) - 1, len(word2) - 1)
 
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
 
-
         return 1
 
     def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [0 for _ in range(amount + 1)]
+        for i in range(1, amount + 1):
+            cost = float('inf')
+            for c in coins:
+                if i - c >= 0:
+                    cost = min(cost, dp[i - c] + 1)
+            dp[i] =cost
+        if dp[amount] ==float('inf'):
+            return -1
+        else:
+            return dp[amount]
 
-        return 0
 
     def test(self):
-        amount =10
+        amount = 10
         dp = [float("inf")] * (amount + 1)
-
+        m = 3
+        n = 4
+        dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
 
         return
 
@@ -45,3 +57,4 @@ if __name__ == '__main__':
     # handler.init()
     # handler.minDistance()
     handler.test()
+    handler.coinChange([1, 2, 5], 11)
