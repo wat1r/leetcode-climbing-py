e8bad08278f2649474b5d1e35f57fb9969c35705
wat1r <cnwangzhou@hotmail.com>
2019-11-24 21:53:11 +0800

    stack

--- leetcode_base/random_leetcode_step_one.py
+++ leetcode_base/random_leetcode_step_one.py
@@ -10,6 +10,12 @@ class TreeNode():
         self.right = None
 
 
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
 class RandomLeetcodeStepOne():
 
     def init(self):
@@ -164,17 +170,146 @@ class RandomLeetcodeStepOne():
         if pRoot1.val != pRoot2.val: return False
         return self.HasSubtreeSegment(pRoot1.left, pRoot2) and self.HasSubtreeSegment(pRoot1.right, pRoot2)
 
-    def firstMissingPositive(self, nums: List[int]) -> int:
-        s = set()
-        n = len(nums)
-        for i in range(0, n):
-            if nums[i] >= 1 and nums[i] <= n + 1:
-                s.add(nums[i])
+    # def firstMissingPositive(self, nums: List[int]) -> int:
+    #     s = set()
+    #     n = len(nums)
+    #     for i in range(0, n):
+    #         if nums[i] >= 1 and nums[i] <= n + 1:
+    #             s.add(nums[i])
+    #
+    #     for i in range(1, n + 1):
+    #         if i not in s:
+    #             return i
+    #     return n + 1
 
-        for i in range(1, n + 1):
-            if i not in s:
-                return i
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        n = len(nums)
+        for i in range(n):
+            while nums[i] >= 1 and nums[i] <= n and nums[i] != nums[nums[i] - 1]:
+                self.swap(nums, i, nums[i] - 1)
+                # nums[i], nums[nums[i] - 1] = nums[nums[i] - 1], nums[i]
+        for i in range(n):
+            if nums[i] != i + 1: return i + 1
         return n + 1
+
+    def swap(self, nums, a, b):
+        nums[a], nums[b] = nums[b], nums[a]
+
+    def isValidBST(self, root: TreeNode) -> bool:
+
+        """
+        :param root:
+        :return:
+        """
+
+        def helper(node, lower=float('-inf'), upper=float('+inf')):
+            if not node: return True
+            val = node.val
+            if val <= lower or val >= upper: return False
+            if not helper(node.left, lower, val): return False
+            if not helper(node.right, val, upper): return False
+            return True
+
+        return helper(root)
+
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        stack, res = [], []
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            res.append(root.val)
+            root = root.right
+        return res
+
+    def isValidBST(self, root: TreeNode) -> bool:
+        stack, pre = [], float('-inf')
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            if pre >= root.val: return False
+            pre = root.val
+            root = root.right
+        return True
+
+    def hasCycle(self, head: ListNode) -> bool:
+        if not head or not head.next: return False
+        slow, fast = head, head.next
+        while fast.next and fast.next.next:
+            if slow == fast: return True
+            slow = slow.next
+            fast = fast.next.next
+        return False
+
+    def detectCycle(self, head: ListNode) -> ListNode:
+        if not head or not head.next: return None
+        slow, fast = head, head.next
+        while fast.next and fast.next.next:
+            if slow == fast: break
+            slow = slow.next
+            fast = fast.next.next
+        if slow != fast: return None
+        while head != slow.next:
+            head = head.next
+            slow = slow.next
+        return head
+
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1 or not l2: return l1 if l1 else l2
+        if l1.val > l2.val:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+        else:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = ListNode(0)
+        node = dummy
+        while l1 and l2:
+            if l1.val > l2.val:
+                node.next = l2
+                l2 = l2.next
+            else:
+                node.next = l1
+                l1 = l1.next
+            # 注意将node移动向下一个
+            node = node.next
+        #     判断了不是null 节点
+        node.next = l1 if l1 is not None else l2
+        return dummy.next
+
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        res = []
+        record = dict()
+        for i in range(len(nums)):
+            remain = target - nums[i]
+            if remain in record:
+                res.append(i)
+                res.append(record[remain])
+                break
+            record[nums[i]] = i
+        return res
+
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        m -= 1
+        n -= 1
+        t = m + n + 1
+        while m >= 0 and n >= 0:
+            if nums1[m] > nums2[n]:
+                nums1[t] = nums1[m]
+                m -= 1
+            else:
+                nums1[t] = nums2[n]
+                n -= 1
+            t -= 1
+        nums1[:n + 1] = nums2[:n + 1]
 
     def test(self):
         # amount = 10
@@ -196,7 +331,7 @@ if __name__ == '__main__':
     handler = RandomLeetcodeStepOne()
     # handler.init()
     # handler.minDistance()
-    handler.test()
+    # handler.test()
     # handler.coinChange([1, 2, 5], 11)
     # node4 = TreeNode(4)
     # node2 = TreeNode(2)
@@ -215,4 +350,7 @@ if __name__ == '__main__':
     # node7.right = node9
     #
     # handler.invertTree(node4)
-    handler.firstMissingPositive([1, 2, 0])
+    # handler.firstMissingPositive([1, 2, 0])
+    # handler.firstMissingPositive([3, 4, -1, 1])
+    # handler.firstMissingPositive([2])
+    handler.firstMissingPositive([])
