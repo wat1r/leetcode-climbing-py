372afe81bd825baca490c74331a26796e6bf4b96
wat1r <cnwangzhou@hotmail.com>
2021-04-29 09:44:51 +0800

    redis test

---
+++ leetcode_base/_15.py
@@ -0,0 +1,34 @@
+import collections
+from bisect import bisect_left, bisect_right
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        counter = collections.Counter(nums)
+        nums = sorted(counter)
+        ret = []
+        for i, num in enumerate(nums):
+            # case i. three numbers are the same - [0,0,0]
+            if num == 0:
+                if counter[num] > 2:
+                    ret.append([0, 0, 0])
+            # case ii. two numbers are the same
+            elif counter[num] > 1 and -2 * num in counter:
+                ret.append([num, num, -2 * num])
+            # case iii. not any of the three numbers are the same
+            if num < 0:
+                opposite = -num
+                left = bisect_left(nums, opposite - nums[-1], i + 1)
+                right = bisect_right(nums, opposite / 2, left)
+                for a in nums[left:right]:
+                    b = opposite - a
+                    if b in counter and a != b:
+                        ret.append([num, a, b])
+        return ret
+
+
+if __name__ == '__main__':
+    nums = [-1, 0, 1, 2, -1, -4]
+    handler = Solution()
+    handler.threeSum(nums)
