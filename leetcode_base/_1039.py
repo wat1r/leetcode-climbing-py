f90bbc8a5bb098a475bd8a4535b770f9ea5b0e00
wangzhou <wangzhou@shengqugames.com>
2023-06-27 08:46:44 +0800

    daily

--- leetcode_base/_1039.py
+++ leetcode_base/_1039.py
@@ -1,5 +1,22 @@
+from linecache import cache
+from math import inf
+from typing import List
+
+
 class _1st:
-    print("")
+    def minScoreTriangulation(self, v: List[int]) -> int:
+        n = len(v)
+
+        @cache
+        def dfs(i, j):
+            if i + 1 == j:
+                return 0
+            res = inf
+            for k in range(i + 1, j):
+                res = min(res, dfs(i, k) + dfs(k, j) + v[i] * v[k] * v[j])
+            return res
+
+        return dfs(0, n - 1)
 
 
 class _2nd:
@@ -7,7 +24,6 @@ class _2nd:
 
 
 class _3rd:
-
     print("")
 
 
