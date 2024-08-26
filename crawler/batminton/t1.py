c050127440b0de20c8323d6a11a9168c3f24a81c
wangzhou <wangzhou@shengqugames.com>
2024-08-26 15:41:44 +0800

    schedule task

--- crawler/batminton/t1.py
+++ crawler/batminton/t1.py
@@ -1,37 +1,167 @@
+import json
+import os
+import sys
+from datetime import datetime, timedelta
+
+import time
+from concurrent.futures import Future, ThreadPoolExecutor
+
+import urllib3
+from apscheduler.schedulers.background import BackgroundScheduler
 import requests
 
-headers = {
-    'Host': 'api.wesais.com',
-    'Connection': 'keep-alive',
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Accept': 'application/json, text/plain, */*',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 MicroMessenger/7.0.20.1781(0x6700143B) NetType/WIFI MiniProgramEnv/Windows WindowsWechat/WMPF WindowsWechat(0x63090b11)XWEB/9193',
-    'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjdCI6MTcyMzg5OTUwMSwiaWQiOiIxNzIzODk5NTAxMDM3NzUiLCJhaWQiOjEwMTAxLCJtaWQiOjQsInRpZCI6NSwicGFyYW1zIjoie1wiY29tcGFueV9pZFwiOjYxNSxcImFjY291bnRfdHlwZVwiOjMxLFwiYWNjb3VudF9pZFwiOjEwMDk2MTYsXCJtZW1iZXJfaWRcIjoxMTkyMjk4LFwic2FmZV9sXCI6MSxcInBlcnNvbm5lbF9pZFwiOjExOTIyOTh9In0.Xr-canCRc4SsWtEMjCnefSCVyenhF24z4Mq6pJLGriI',
-    'Origin': 'https://xcx.wesais.com',
-    'Sec-Fetch-Site': 'same-site',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Dest': 'empty',
-    'Referer': 'https://xcx.wesais.com/',
-    # 'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'zh-CN,zh;q=0.9',
+# 关掉不安全证书的警告
+urllib3.disable_warnings()
+
+target_list = {
+    "date": ["2024-08-28"],
+    "match": ["17:00--18:00", "18:00--19:00", "19:00--20:00", "20:00--21:00", "21:00--22:00"]
 }
 
-time_date = "2024-08-20"
+warning_info = dict()
 
-data = {
-    'business_id': '10000785',
-    'stadium_id': '11501',
-    'ground_id': '11501001',
-    'time_date': time_date,
-    'request_id': 'e0915de185431a17d047d1601e11fac0',
-}
 
-response = requests.post('https://api.wesais.com/field/wxFieldBuyPlan/getList', headers=headers, data=data,
-                         verify=False)
-result = response.json()
-sku_list = result['data']['skuList']
-for sku in sku_list:
-    for sku_item in sku:
-        for s in sku_item:
-            print(s['sku_name'] + "场次:" + time_date + " " + s['time_str'] + ("已定" if s['is_lock'] else "空闲"))
-print(result)
+def detect_sku(time_date: str = None):
+    #
+    if not time_date:
+        time_date = build_date(interval=2)
+    init()
+    headers = {
+        'Host': 'api.wesais.com',
+        'Connection': 'keep-alive',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'application/json, text/plain, */*',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 MicroMessenger/7.0.20.1781(0x6700143B) NetType/WIFI MiniProgramEnv/Windows WindowsWechat/WMPF WindowsWechat(0x63090b19)XWEB/11177',
+        'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjdCI6MTcyNDY1MjY4OSwiaWQiOiIxNzI0NjUyNjg5MDIyNDIiLCJhaWQiOjEwMTAxLCJtaWQiOjQsInRpZCI6NSwicGFyYW1zIjoie1wiY29tcGFueV9pZFwiOjYxNSxcImFjY291bnRfdHlwZVwiOjMxLFwiYWNjb3VudF9pZFwiOjEwMDk2MTYsXCJtZW1iZXJfaWRcIjoxMTkyMjk4LFwic2FmZV9sXCI6MSxcInBlcnNvbm5lbF9pZFwiOjExOTIyOTh9In0.DgwwCi28oxH1UzfW_gOb8oTQx4Og3H6aoAPyeqPDhcA',
+        'Origin': 'https://xcx.wesais.com',
+        'Sec-Fetch-Site': 'same-site',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Dest': 'empty',
+        'Referer': 'https://xcx.wesais.com/',
+        # 'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'zh-CN,zh;q=0.9',
+    }
+    time_date = '2024-08-28'
+    data = {
+        'business_id': '10000785',
+        'stadium_id': '11501',
+        'ground_id': '11501001',
+        'time_date': time_date,
+        'request_id': 'd43e35b57d5a6a28c46bc77d4fdd00ef',
+    }
+    print(f"detect time_date:{time_date}")
+    if time_date not in target_list['date']:
+        print(f"{time_date} is not in candidate date")
+        return
+    response = requests.post('https://api.wesais.com/field/wxFieldBuyPlan/getList', headers=headers, data=data,
+                             verify=False)
+    result = response.json()
+    if "code" not in result or result['code'] != 200:
+        print(f"result----->{result}")
+        return
+    collect_info = []
+    try:
+        sku_list = result['data']['skuList']
+        for sku in sku_list:
+            for sku_item in sku:
+                for s in sku_item:
+                    if time_date in target_list['date']:
+                        if not s['is_lock'] and s['time_str'] in target_list['match']:
+                            collect_info.append(
+                                time_date + "_" + s['sku_name'] + "#" + s['time_str'] + "#" + ("已定" if s[
+                                    'is_lock'] else "空闲"))
+                            print(s['sku_name'] + "场次:" + time_date + " " + s['time_str'] + (
+                                "已定" if s['is_lock'] else "空闲"))
+    except Exception as ex:
+        print(f"detect_sku--->{ex}")
+    if collect_info is not None and len(collect_info) > 0:
+        content = """
+    ### **南部市民中心**
+    """
+        for item in collect_info:
+            v = item.split("#")
+            content += create_warn_content(field=v[0], match=v[1], field_status=v[2])
+        if content and content != "":
+            send_weixin(content)
+            today = datetime.now().strftime('%Y-%m-%d')
+            if today not in warning_info:
+                warning_info[today] = 0
+            warning_info[today] += 1
+            print(f"warning_info:{warning_info}")
+            if warning_info[today] >= 3:
+                print("warning_info has already send 3 times,quit")
+                sys.exit()
+
+
+def create_warn_content(field: str = "", match: str = "", field_status: str = ""):
+    warnContent = """
+    > 场地:<font color="info">""" + field + """</font> 
+    > 场次:<font color="warning">""" + match + " " + field_status + """</font> 
+    """
+    return warnContent
+
+
+def init():
+    today = datetime.now()
+    today_weekday = today.weekday()
+    if today_weekday == 5:
+        days_to_saturday = 7
+    else:
+        days_to_saturday = (5 - today_weekday) % 7
+    next_saturday = today + timedelta(days=days_to_saturday)
+    formatted_next_saturday = next_saturday.strftime('%Y-%m-%d')
+    global target_list
+    # target_list['date'].clear()
+    target_list['date'].append(formatted_next_saturday)
+    print(f"target_list---->{target_list}")
+
+
+# warnContent = bytes(warnContent, 'utf-8').decode('unicode_escape')
+
+def send_weixin(content):
+    webHookUrl = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=1601b78b-eb16-43c8-8afa-04a3362b0746"
+    headers = {"Content-Type": "application/json"}
+    data = {
+        "msgtype": "markdown",
+        "markdown": {
+            "content": content,  # 让群机器人发送的消息内容。
+            "mentioned_list": "@all",
+        }
+    }
+    data = json.dumps(data)
+    res = requests.post(webHookUrl, data=data, headers=headers, verify=False)  # 直接一句post就可以实现通过机器人在群聊里发消息
+
+
+def build_date(interval: int):
+    current_date = datetime.now()
+    interval_days = timedelta(days=interval)
+    future_date = current_date + interval_days
+    formatted_future_date = future_date.strftime('%Y-%m-%d')
+    return formatted_future_date
+
+
+def start_job():
+    print("start_job_core start")
+    start_job_core()
+
+
+def start_job_core():
+    print("------------------timer:start------------------")
+    # 创建后台调度器
+    scheduler = BackgroundScheduler()
+    # 添加任务，interval参数表示间隔时间，单位为秒
+    scheduler.add_job(detect_sku, 'interval', seconds=60 * 1)
+    # 启动调度器
+    scheduler.start()
+    # 为了防止程序退出，主线程在这里等待
+    try:
+        while True:
+            time.sleep(2)
+    except KeyboardInterrupt:
+        # 关闭调度器
+        scheduler.shutdown()
+
+
+if __name__ == '__main__':
+    start_job()
+    # detect_sku()
---
+++ fund_project/txlc/get_furecom_unified_query_dp_message.py
@@ -0,0 +1,96 @@
+import datetime
+import json
+
+import mitmproxy.http as http
+from mitmproxy import ctx
+from urllib.parse import urlparse, parse_qs
+
+
+
+class HTTPRecordModifier:
+
+    def __init__(self, flow: http.HTTPFlow):
+        self.flow = flow
+
+    # 设置请求头信息
+    def set_request_header(self, headers):
+        for header_key, header_value in headers.items():
+            self.flow.request.headers[header_key] = header_value
+
+    # 设置请求 body 参数
+    def set_request_body(self, body):
+        self.flow.request.content = bytes(body, "utf-8")
+
+    # 设置请求方法
+    def set_request_method(self, method):
+        self.flow.request.method = method
+
+    # 设置请求 query 参数
+    def set_request_query(self, key, value):
+        self.flow.request.query[key] = value
+
+    # 设置响应状态码
+    def set_response_status_code(self, code):
+        self.flow.response.status_code = code
+
+    # 设置响应头信息
+    def set_response_header(self, headers):
+        for header_key, header_value in headers.items():
+            self.flow.response.headers[header_key] = header_value
+
+    # 设置响应体内容
+    def set_response_body(self, body):
+        self.flow.response.content = bytes(body, "utf-8")
+
+    # 构造响应报文
+    def create_mocked_response(self, code=200, header={}, body=""):
+        self.flow.response = http.HTTPResponse.make(code, bytes(body, "utf-8"), header)
+
+
+class Test:
+
+    def check_field_in_url(self, url, key):
+        # 解析URL
+        parsed_url = urlparse(url)
+        # 获取查询字符串部分
+        query_components = parse_qs(parsed_url.query)
+        # 检查字段是否存在
+        return key in query_components
+
+    def response(self, flow: http.HTTPFlow):
+        current_date = datetime.datetime.now().strftime('%Y%m%d')
+        """
+        Event for handling response before sending it back to the client
+        """
+        # 这里编写我们的 mock 逻辑代码
+        breakpoint_url = "www.tencentwm.com/"
+        url = flow.request.pretty_url
+        if str(url).__contains__(breakpoint_url):
+            print("================url->", url)
+            cookies = flow.request.cookies
+            print("cookies:", cookies)
+            self.append_data(f"./{current_date}_cookies", str(cookies) + "\n")
+            response_content = json.loads(flow.response.content.decode("utf-8"))
+            print("=========response_content", response_content)
+            response_content['total'] = 20
+            new_response = HTTPRecordModifier(flow)
+            print("new_response", new_response)
+            userinfo = {
+                # 这里放置上面抓包获取的用户信息格式
+            }
+
+            # for i in range(response_content['total']):
+            #     response_content['users'].append(userinfo)
+            # new_response.set_response_body(json.dumps(response_content))
+
+    def append_data(self, file_path, data):
+        try:
+            with open(file_path, 'a') as file:
+                file.write(data)
+        except IOError as e:
+            print(f"An error occurred while appending to the file: {e}")
+
+
+addons = [
+    Test()
+]
